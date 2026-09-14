017c5e96d33a70717a9890badfcab3a3052ac220
Valentyn Kit <valentynkit.work@pm.me>
2026-09-14 15:41:32 +0300

    sql: parse where condition

--- src/commands/sql_query.rs
+++ src/commands/sql_query.rs
@@ -1,4 +1,4 @@
-use std::{fmt::format, fs::File};
+use std::{collections::HashMap, fmt::format, fs::File};
 
 use anyhow::bail;
 use tracing::{debug, info, warn};
@@ -10,12 +10,126 @@ use crate::{
     error::{FormatError, QueryError},
 };
 
+pub type Result<T, E = QueryError> = core::result::Result<T, E>;
+
+/// Used for parsing, during iteration to identify what is the current query section.
+#[derive(Debug, PartialEq, Eq)]
+enum QuerySection {
+    Select,
+    From,
+    Where,
+}
+
+impl QuerySection {
+    // State machine, moving to next query section
+    fn next(self, query: &str) -> Result<Self> {
+        match self {
+            Self::Select => Ok(Self::From),
+            Self::From => Ok(Self::Where),
+            Self::Where => Err(QueryError::Malformed {
+                query: query.to_owned(),
+                reason: "WHERE is the last query section".to_owned(),
+            }),
+        }
+    }
+
+    fn validate(&self, query: &str, token: &str) -> Result<()> {
+        let (section, forbidden): (&str, &[&str]) = match self {
+            Self::Select => ("SELECT", &["select", "where"]),
+            Self::From => ("FROM", &["select", "from"]),
+            Self::Where => ("WHERE", &["select", "from", "where"]),
+        };
+        if forbidden.contains(&token) {
+            return Err(QueryError::Malformed {
+                query: query.to_owned(),
+                reason: format!("{section} section shouldn't contain {token}"),
+            });
+        }
+        Ok(())
+    }
+}
+
+fn parse_query<'a>(
+    query: &'a str,
+    tokens: &[&'a str],
+    what: &mut Vec<&'a str>,
+    from: &mut Vec<&'a str>,
+    conditions: &mut Vec<(&'a str, &'a str)>,
+) -> Result<()> {
+    let mut query_section = QuerySection::Select;
+    let mut idx: i32 = -1;
+
+    let malformed = |reason: &str| QueryError::Malformed {
+        query: query.to_owned(),
+        reason: reason.to_owned(),
+    };
+
+    for &token in tokens {
+        idx += 1;
+        query_section.validate(query, token)?;
+        match query_section {
+            QuerySection::Select => {
+                if token == "from" {
+                    if what.is_empty() {
+                        return Err(malformed(
+                            "expected > 0 items after SELECT, got 0. `SELECT FROM ...`",
+                        ));
+                    }
+                    query_section = query_section.next(query)?;
+                    continue;
+                }
+                what.push(token);
+            }
+            QuerySection::From => {
+                if token == "where" {
+                    if from.is_empty() {
+                        return Err(malformed(
+                            "expected > 0 items after FROM, got 0. `SELECT <...> FROM WHERE`",
+                        ));
+                    }
+                    query_section = query_section.next(query)?;
+                    idx += 1;
+                    break;
+                }
+                from.push(token);
+            }
+            QuerySection::Where => {
+                return Err(malformed("unknown section during parsing"));
+            }
+        }
+    }
+
+    if query_section == QuerySection::Where {
+        if idx as usize == &tokens.len() - 1 {
+            return Err(malformed(
+                "expected `SELECT <expr> FROM <table> WHERE <condition>`, has missing conditions after WHERE",
+            ));
+        }
+        for chunk in tokens[(idx as usize)..].chunks(3) {
+            let [col, condition, value] = chunk else {
+                return Err(malformed("WHERE expects `<col> <op> <value>`"));
+            };
+            warn!(?col, ?condition, ?value, "parsed query chunk");
+            if *condition != "=" {
+                return Err(malformed("WHERE expects `<col> = <value>`"));
+            }
+
+            conditions.push((col, value));
+
+            debug!(?col, ?condition, ?value, "parsing WHERE section");
+        }
+    }
+
+    debug!(?what, ?from, ?idx, ?query_section, "parsed SQL query");
+    Ok(())
+}
+
 pub(crate) fn run(
     file: &File,
     schemas: Vec<SqliteSchema>,
     db_hdr: DbHeader,
     query: Vec<String>,
-) -> Result<String, QueryError> {
+) -> Result<String> {
     assert_eq!(query.len(), 1, "query should be one element");
     let query = (&query[0]).to_owned().to_ascii_lowercase();
 
@@ -30,18 +144,28 @@ pub(crate) fn run(
         .ok_or(malformed("expected `SELECT <...>"))?;
 
     let tokens: Vec<&str> = query.split_whitespace().collect();
-    let [what @ .., from, tbl_name] = tokens.as_slice() else {
-        return Err(malformed("expected `SELECT <expr> FROM <table>`"));
-    };
+    let mut what: Vec<&str> = vec![];
+    let mut from: Vec<&str> = vec![];
+    // could be empty, column = expected value.
+    let mut conditions: Vec<(&str, &str)> = vec![];
 
-    if !from.eq_ignore_ascii_case("from") {
-        return Err(malformed("expected SELECT"));
+    if tokens.len() <= 2 {
+        return Err(malformed("expected query to have more than 4 words"));
     }
+
+    parse_query(query, &tokens, &mut what, &mut from, &mut conditions)?;
+    if what.is_empty() {
+        return Err(malformed("expected SELECT <expr> "));
+    }
+    if from.len() != 1 {
+        return Err(malformed("expected FROM <tbl_name> "));
+    }
+    let tbl_name = from[0];
 
     let table = schemas
         .iter()
-        .find(|s| s.tbl_name() == *tbl_name)
-        .ok_or(QueryError::NoSuchTable((*tbl_name).to_owned()))?;
+        .find(|s| s.tbl_name() == tbl_name)
+        .ok_or(QueryError::NoSuchTable((tbl_name).to_owned()))?;
 
     debug!(?table);
     let page_size = db_hdr.page_size();
@@ -52,7 +176,25 @@ pub(crate) fn run(
 
     for &ptr in page_header.cell_pointers() {
         let (cell, _) = parse_cell(&page_buf[(ptr as usize)..])?;
-        cells.push(cell);
+        let mut meet_conditions = false;
+        for (col, expected_value) in &conditions {
+            let Some(&col_idx) = table.sql_parsed().get(*col) else {
+                return Err(QueryError::NoSuchColumn((*col).to_owned()));
+            };
+
+            let actual_value = cell
+                .record
+                .values
+                .get(col_idx)
+                .ok_or(QueryError::NoSuchColumn((*col).to_owned()))?;
+
+            if &actual_value.to_string() == expected_value {
+                meet_conditions = true;
+            }
+        }
+        if meet_conditions {
+            cells.push(cell);
+        }
     }
 
     if what.len() == 1 && what[0].eq_ignore_ascii_case("count(*)") {
